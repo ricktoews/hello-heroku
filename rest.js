434db2a72e5cbb8583362abbdad0b91ccd335de2
Rick Toews <rick@toewsweb.net>
2015-05-26 20:13:33 -0400

    Add files needed for Heroku. I think. x

--- rest.js
+++ rest.js
@@ -1,33 +1,42 @@
 //===========================================================================================
 // Equivalence REST Interface
 //===========================================================================================
-var restify = require('restify');
+var restify = require("restify");
 
-var arithmo = require('arithmo');
+var arithmo = require("arithmo");
+
 //=======================================================
 // Create the REST server
 //=======================================================
 var server = restify.createServer({});
 
 server.use(restify.bodyParser({rejectUnknown: true}));  // Pulls out the parameters
+server.use(restify.CORS());
+server.use(restify.fullResponse());
 server.use(restify.jsonp());
 server.listen(8000);
 
 // Routes
-server.get('/hello', hello);
-server.get('/phi', phi);
-//server.post('/phi', phi);
-
-function hello(req, res, callback) {
-    console.log("Hello");    
-    res.contentType = 'json';
-    var test = arithmo.test();
-    res.json(test);
-}
+server.get("/phi/:power", phi);
+server.get("/dc/:denom", dc);
+server.post("/phi", phi);
 
 function phi(req, res, callback) {
-    res.contentType = 'json';
-    var data = arithmo.phiPowers(3);
+	if (req.method == "POST")
+		power = req.body.power;
+	else
+		power = req.params.power;
+    res.contentType = "json";
+    var data = arithmo.phiPowers(power);
     res.json(data);
     return callback();
 }
+
+function dc(req, res, callback) {
+	var denom = req.params.denom;
+	var data = arithmo.decimals(denom);
+	res.json(data);
+	return callback();
+}
+
+exports.server = server;
