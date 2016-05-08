4dfb762d2db1fb8a7142d18f7242962926f3964e
Rick Toews <rick@toewsweb.net>
2016-05-08 03:08:59 -0400

    Separate stuff.

---
+++ app/decimal_controller.js
@@ -0,0 +1,12 @@
+angular.module('demo')
+.controller('DecimalCtrl', function($scope, decimal) {
+  $scope.calculate = function() {
+    decimal($scope.denom).then(function(response) {
+      var data = response.data;    console.log("decimal data", data);
+      $scope.decimalData = data;
+    
+    });
+  }
+})
+;
+
