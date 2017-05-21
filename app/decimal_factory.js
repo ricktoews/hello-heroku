564b4ddfbda0ee74de4e050fddef830a10c9bb81
retorick <rick@toewsweb.net>
2017-05-21 00:13:47 +0000

    Remove http, in a first attempt to fix Mixed Content issue.

--- app/decimal_factory.js
+++ app/decimal_factory.js
@@ -2,7 +2,7 @@ angular.module('mathToysApp')
 .factory('decimal', function($http) {
     console.log('making decimal factory');
     return function(denom) {
-        var url = 'http://arithmo-rest.toewsweb.net/dc/' + denom;
+        var url = '//arithmo-rest.toewsweb.net/dc/' + denom;
         return $http.get(url);
         
     }
